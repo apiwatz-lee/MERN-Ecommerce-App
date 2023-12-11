ee506efd4c3a9d0aaba4105533355cb06ef1c604
Apiwat Lee <apiwatlee@Apiwats-MacBook-Air.local>
2023-12-12 03:06:12 +0700

    feat:add product list layout

---
+++ client/src/pages/UploadProduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UploadProduct = () => {
+  return (
+    <main className='font-poppins'>
+        <h1>UploadProduct Page</h1>
+    </main>
+  )
+}
+
+export default UploadProduct
