fff88c1d31b3780089730e2ca42c70c23cabddae
Apiwat Lee <apiwatlee@Apiwats-MacBook-Air.local>
2023-12-13 00:33:29 +0700

    feat:connect mongodb database

--- server/app.js
+++ server/app.js
@@ -1,12 +1,34 @@
 import express from "express"
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import { client } from "./utils/db.js";
 
-const app = express();
-const port = 4000;
+async function init(){
+    
+    dotenv.config();
+    const app = express();
+    app.use(cors())
+    app.use(bodyParser.json());
+    const port = 4002;
+    await client.connect();
+    
+    app.get("/products",(req,res)=>{
+        res.send('This is get products api')
+    })
+    
+    app.get("/",(req,res)=>{
+        res.send('test')
+    })
+    
+    app.get("*", (req, res) => {
+        res.status(404).send('Not found endpoint');
+    });
 
-app.get("/", (req,res)=>{
-    res.send('Hello Apiwatz Lee');
-})
+    app.listen(port,()=>{
+        console.log(`Server is running at ${port}`)
+    })
+}
 
-app.listen(port,()=>{
-    console.log(`Server is running at ${port}`)
-})
+init();
+
