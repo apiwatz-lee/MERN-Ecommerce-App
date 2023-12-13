d8561017c598bb9e045fd640ef28d821753bf420
Apiwat Lee <apiwatlee@Apiwats-MacBook-Air.local>
2023-12-13 19:27:56 +0700

    feat:client can drag and drop to upload image zone

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,6 @@ import './App.css'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import ProductListPage from './pages/ProductListPage'
 import UploadProductPage from './pages/UploadProductPage'
-import { PrimeReactProvider } from 'primereact/api';
-import 'primereact/resources/themes/lara-light-indigo/theme.css';   
-import 'primeflex/primeflex.css';                                   
-import 'primeicons/primeicons.css';
-import 'primereact/resources/primereact.css';
 import Navigator from './components/Navigator'
 
 
@@ -17,15 +12,13 @@ function App() {
 
   return (
     <>
-        <PrimeReactProvider>
-          <BrowserRouter>
-            <Navigator/>
-            <Routes>
-              <Route path='/products' element={<ProductListPage/>}/>
-              <Route path='/upload' element={<UploadProductPage/>}/>
-            </Routes>
-          </BrowserRouter>
-        </PrimeReactProvider>
+      <BrowserRouter>
+        <Navigator/>
+        <Routes>
+          <Route path='/products' element={<ProductListPage/>}/>
+          <Route path='/upload' element={<UploadProductPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Button = ({type,title}) => {
-  console.log(type);
+  
   return    <button type={type} className={`border p-3 rounded-full w-28 lg:w-56 ${title === 'ยืนยัน' ? `text-white bg-[#E04132]`:`bg-white text-[#E04132]`}`}>
                 {title}
             </button>  
