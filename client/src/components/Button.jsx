6acd64f1689fefc5049e05e04b4f1ab7e29f3035
Apiwat Lee <apiwatlee@Apiwats-MacBook-Air.local>
2023-12-13 22:24:21 +0700

    feat:develop validation limitation of  numbers and type of image to be uploaded

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const Button = ({type,title}) => {
+const Button = ({type,title,onClick}) => {
   
-  return    <button type={type} className={`border p-3 rounded-full w-28 lg:w-56 ${title === 'ยืนยัน' ? `text-white bg-[#E04132]`:`bg-white text-[#E04132]`}`}>
+  return    <button 
+                  type={type} 
+                  className={`border p-3 rounded-full w-28 lg:w-56 ${title === 'ยืนยัน' ? `text-white bg-[#E04132]`:`bg-white text-[#E04132]`}`}
+                  onClick={onClick}
+                  >
                 {title}
             </button>  
 }
