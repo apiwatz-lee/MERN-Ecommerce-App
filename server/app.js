9221f9ec8e735e90e84f974257142e77326e64bc
Apiwat Lee <apiwatlee@Apiwats-MacBook-Air.local>
2023-12-17 18:09:59 +0700

    server:update port env

--- server/app.js
+++ server/app.js
@@ -100,7 +100,7 @@ async function init(){
         res.status(404).send('Not found endpoint');
     });
 
-    app.listen(port,()=>{
+    app.listen(process.env.PORT || 4000,()=>{
         console.log(`Server is running at ${port}`)
     })
 }
