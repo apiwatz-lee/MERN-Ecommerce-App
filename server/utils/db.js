ac564aba12332370544933d88c415bd77607a928
Apiwat Lee <apiwatlee@Apiwats-MacBook-Air.local>
2023-12-17 17:30:48 +0700

    feat:config server deploy on render successfully

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,6 +1,8 @@
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
-const connectionString = "mongodb+srv://apiwatzlee:Apiwatz8899@cluster0.icecmhn.mongodb.net/?retryWrites=true&w=majority";
+const connectionString = process.env.MONGO_CONNECTION;
 
 export const client = new MongoClient(connectionString, {
     useUnifiedTopology: true,
