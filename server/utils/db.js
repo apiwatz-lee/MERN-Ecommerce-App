c6b84c7899ce781a1cadb424216e88273ace1add
Apiwat Lee <apiwatlee@Apiwats-MacBook-Air.local>
2023-12-17 17:03:52 +0700

    feat:connect mongoDB atlas on cloud

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const connectionString = "mongodb://localhost:27017";
+const connectionString = "mongodb+srv://apiwatzlee:Apiwatz8899@cluster0.icecmhn.mongodb.net/?retryWrites=true&w=majority";
 
 export const client = new MongoClient(connectionString, {
     useUnifiedTopology: true,
